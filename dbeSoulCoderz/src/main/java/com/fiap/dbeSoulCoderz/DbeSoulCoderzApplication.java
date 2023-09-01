bd1f8e365ebe2bc048e45ef732eae48d01436a7a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-01 09:26:48 -0300

    Criando projeto

---
+++ dbeSoulCoderz/src/main/java/com/fiap/dbeSoulCoderz/DbeSoulCoderzApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.dbeSoulCoderz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbeSoulCoderzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbeSoulCoderzApplication.class, args);
+	}
+
+}
---
+++ dbeSoulCoderz/src/test/java/com/fiap/dbeSoulCoderz/DbeSoulCoderzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.dbeSoulCoderz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbeSoulCoderzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
